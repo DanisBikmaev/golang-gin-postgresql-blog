832a17c2eebe4b66f03d68a3e187dfef5fa46953
DanisBikmaev <bikmaevdv@gmail.com>
2023-04-06 18:50:28 +0500

    db user CRUD generation

---
+++ db/query/user.sql
@@ -0,0 +1,27 @@
+-- name: CreateUser :one
+INSERT INTO users (
+  username
+) VALUES (
+  $1
+)
+RETURNING *;
+
+-- name: GetUser :one
+SELECT * FROM users
+WHERE id = $1
+LIMIT 1;
+
+-- name: ListUsers :many
+SELECT * FROM users
+ORDER BY username;
+
+-- name: UpdateUser :one
+UPDATE users
+SET username = $2
+WHERE id = $1
+RETURNING *;
+
+
+-- name: DeleteUser :exec
+DELETE FROM users
+WHERE id = $1;
